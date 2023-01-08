e80afb3b055aa2e61d18a8fffba7251b38e2f45d
ToanFX15903 <vtv5hdcq@gmail.com>
2023-01-08 16:35:29 +0700

    FeatureComponent Client

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import usersRoute from "./routes/users.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 const app = express();
 dotenv.config();
@@ -26,7 +26,8 @@ mongoose.connection.on("disconnect", () => {
 });
 
 //middleware routes
-app.use(cookieParser())
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
